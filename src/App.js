0379ec51f27fb1f70877c8e5eeb3037558f465b7
Lorena Di Stefano <lore.distefano@gmail.com>
2022-12-03 19:32:57 -0300

    Counter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import './App.css';
-import ItemListContainer from './Components/ItemListContainer';
+import { ItemListContainer } from './Components/ItemListContainer';
+
 import NavBar from './Components/NavBar';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <ItemListContainer greeting={"No dejes para mañana lo que puedas leer hoy!"} />
+      <ItemListContainer/>
+   
     </div>
   );
 }
--- src/Components/ItemListContainer.jsx
+++ src/Components/ItemListContainer.jsx
@@ -1,11 +1,34 @@
-import React from "react";
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { books } from '../Assets/books'
+import { ItemList } from './ItemList'
 
-const ItemListContainer = ({greeting}) => {
+export const ItemListContainer = () => {
+
+    const [bookList, setBookList] = useState([])
+    const { id } = useParams
+
+    useEffect(() => {
+        const data = new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(id ? books.filter(book => book.gender === id) : books)
+            },2000)
+        })
+
+        data.then((data) => {
+            setBookList(data)
+        })
+    }, [id])
+
+   
+
     return (
-        <div className = "greeting">
-            <h1 >{greeting}</h1>
+        <div>
+            <ItemList bookList={bookList} />
         </div>
     )
+
+
 }
 
-export default ItemListContainer
+
