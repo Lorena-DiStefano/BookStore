d8f5f7c9d33b1329b4541ba883b63f6e450c2356
Lorena Di Stefano <lore.distefano@gmail.com>
2022-12-20 00:48:08 -0300

    Context funcionando

--- src/Components/Cart/CartDetail.jsx
+++ src/Components/Cart/CartDetail.jsx
@@ -1,47 +1,56 @@
 import './Cart.css';
 import { useContext } from 'react'
 import { CartContext } from '../../Context/CartContext';
-import { ItemCounter } from './ItemCounter';
+
 
 
 export const CartDetail = () => {
 
-  const { cartList, fullPayment } = useContext(CartContext)
+  const { cartList, fullPayment, removeItem, cleanCart } = useContext(CartContext)
   console.log(cartList)
 
   return (
     <div className='cartDetail'>
-      <div className='cart'>
-        <table >
-          <thead>
-            <tr >
-              <th scope="col" className="col-1"></th>
-              <th scope="col" className="col-2">Tu Compra</th>
-              <th scope="col" className="col-2">Precio</th>
-              <th scope="col" className="col-2">Cantidad</th>
-              <th scope="col" className="col-2">Total</th>
-            </tr>
-          </thead>
-          {cartList.map(item => (
-            <tbody key={item.id}>
-              <tr className='cartBody' >
-                <th scope="row">
-                  <button type="button"><i className="bi bi-trash"></i></button>
-                </th>
-                <td><img src={item.image} alt={item.title} /></td>
-                <td>$ {(item.price).toLocaleString()}</td>
-                <td className='text-center'>{item.quantity}</td>
-                <td>$ {(item.quantity * item.price).toLocaleString()}</td>
-             
-              </tr>
-            </tbody>
-          ))}
-
-        </table>
-      </div>
-    </div>
+      {cartList.length === 0 ?
+        <p>Tu carrito está vacío</p> :
+        <>
+                
+          <div className='cart' >
+            {cartList.map(item => (
+              <div className='cartItem' key={item.id}>
+                <div>
+                  <button type="button" onClick={() => removeItem(item.id)}><i className="bi bi-trash"></i></button>
+                </div>
+                <div>
+                  <img src={item.image} alt={item.title} />
+                </div>
+                <div>
+                  <h6>Precio</h6>
+                  <p className='text-end'> $ {(item.price).toLocaleString()}</p>
+                </div>
+                <div>
+                  <h6>Cantidad</h6>
+                  <p className='text-center'> {item.quantity}</p>
+                </div>
+                <div>
+                  <h6>Total Item</h6>
+                  <p className='text-end'> $ {(item.quantity * item.price).toLocaleString()}</p>
+                </div>
+              </div>
+            ))}
+            <div className="cleanCart">
+              <button type='button' onClick={cleanCart}><i className="bi bi-cart-x-fill"></i></button>
+            </div>
+            <hr />
+            <div>
+              <h5><b>Total a pagar: </b>$ {fullPayment().toLocaleString()}</h5>
+              <button className="finCompra">Finalizar Compra</button>
+            </div>
+          </div>
+        </>
+      }
+    </div >
   )
 }
 
 export default CartDetail
-
